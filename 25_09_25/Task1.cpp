b2eaad09470ac3e94fd539861361bdd618923920
adham syahmi nordin <adhamsyahmi2004@gmail.com>
2025-10-16 15:10:41 +0800

    commit 2

---
+++ 25_09_25/Task1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+struct Student {
+    int id;
+    string name;
+    string email;
+};
+
+int main()
+{
+    Student student[5];
+
+    for (int i = 0; i < 5; i++) {
+        cout<< "Student " << i+1 << "\nStudent id: ";
+        cin >> student[i].id;
+        cout<< "Student name: ";
+        cin >> student[i].name;
+        cout<< "Student email: ";
+        cin >> student[i].email;
+        cout<< "\n";
+    }
+
+
+    for (int i = 0; i < 5; i++) {
+        cout << student[i].id << '\n' << student[i].name << '\n' << student[i].email << "\n\n";
+    }
+
+    return 0;
+}
